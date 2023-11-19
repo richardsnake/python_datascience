d7e37ce009336d750c71d08fbb19f815d55e7f1f
Richard Abanto <richardabanto@MacBook-Pro-de-Richard-8662.local>
2023-11-19 13:03:04 -0500

    subiendo archivos de clase introducctoria

---
+++ clase1/intro.py
@@ -0,0 +1,71 @@
+print("Hola mundo")
+
+
+# a continuacion voy a solicitar la fecha
+
+#fecha = input("ingrese su fecha de nacimiento: ")
+#print(fecha)
+#print("la fecha ingresada es: "+fecha)
+num1 = 5
+num3 = ""
+
+#print(num1+num3)
+num2 = 7.2
+primer_nombre = "Richard"
+print(primer_nombre + num3)
+_5dias = 0
+dias=4
+diaS="hola"
+print(dias)
+print(diaS)
+v = True
+f = False
+print("operadores numericos")
+print(5/2)
+print(5//2)
+print(6%2)
+print(5.2/2.6)
+print(3*2)
+print(3**3)
+print("operadores comparativos")
+print(5>2)
+print(5<2)
+print(10==10)
+print(10=="10")
+print(10!="10")
+print(10>=9)
+print(10>=10)
+print("operadores logicos and")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print("operadores logicos or")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print("operadores logicos not")
+print(not True)
+print(not False)
+print("conversion implicita")
+print(5+2.0)
+print(5.0+2.0)
+print(5+2)
+print("conversion explicita")
+#num4 = input("ingrese numero: ")
+#print(num4 + str(5))
+#"5"
+edad = 36
+print("Mi edad es "+str(edad))
+print("operaciones con Strings")
+print("Richard - "*3)
+print(primer_nombre)
+print(primer_nombre[0])
+print(primer_nombre[1:4])
+print(primer_nombre[1:5])
+print(primer_nombre[1:6])
+
+print('x' in primer_nombre)
+
+
