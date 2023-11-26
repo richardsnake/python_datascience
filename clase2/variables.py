53524c474025a13ca066c37e9b9f08544a7f0e9e
Richard Abanto <richardabanto@MacBook-Pro-de-Richard-8662.local>
2023-11-26 10:36:05 -0500

    subiendo variables

---
+++ clase2/variables.py
@@ -0,0 +1,67 @@
+
+edad=35
+talla=1.73
+nombre="Richard"
+edad2="35"
+peso="95.8"
+esVaron=True
+esCasado=False
+print("Hola "+nombre+", tu edad es: "+edad2)
+print(edad+talla)
+print(edad+int(edad2))
+print("La talla es: "+str(talla))
+print(talla + float(peso))
+print(oct(11))
+
+#operaciones
+num1=20
+num2=6
+print("operaciones con numeros: ")
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1%num2)
+print(num1**num2)
+print("operaciones con cadena de caracteres: ")
+cadena1="hola "
+cadena2="que tal, como estas?"
+car="k"
+print(cadena1+" "+cadena2)
+print(cadena1*5)
+print(cadena1[4])
+print(cadena2[7:20])
+print("o" in cadena1)
+print("o" not in cadena1)
+
+print("operaciones comparativas: ")
+
+var1=5
+var2=2
+print(var1<var2)
+print(var1>var2)
+print(var1<=var2)
+print(var1>=var2)
+print(var1==var2)
+print(var1!=var2)
+
+print("operaciones logicas: ")
+
+verdad=True
+mentira=False
+print("operaciones logicas: and")
+print(False and False)
+print(True and False)
+print(False and True)
+print(True and True)
+print("operaciones logicas: or")
+print(False or False)
+print(True or False)
+print(False or True)
+print(True or True)
+print("operaciones logicas: not")
+print(verdad)
+print(mentira)
+print(not verdad)
+print(not mentira)
